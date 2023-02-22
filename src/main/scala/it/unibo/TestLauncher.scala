52309a17f86915259f62bf8c41abac7c1435c025
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-02-22 16:26:16 +0100

    chore: really bad solution for alchemist control

--- src/main/scala/it/unibo/TestLauncher.scala
+++ src/main/scala/it/unibo/TestLauncher.scala
@@ -5,17 +5,19 @@ import it.unibo.alchemist.core.implementations.Engine
 import it.unibo.alchemist.core.interfaces.Status
 import it.unibo.alchemist.loader.LoadAlchemist
 import it.unibo.alchemist.model.implementations.molecules.SimpleMolecule
-import it.unibo.alchemist.model.interfaces.{Environment, Position, Actionable, Time}
+import it.unibo.alchemist.model.interfaces.{Actionable, Environment, Position, Time}
+
 import java.io.File
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.{Semaphore, TimeUnit}
+import java.util.concurrent.locks.ReentrantLock
 import scala.jdk.CollectionConverters.IteratorHasAsScala
 import scala.language.existentials
 
 object TestLauncher extends App {
 
-  val file = new File("/Users/davidedomini/Desktop/prova-scafi-alchemist/src/main/yaml/simple.yml")
+  val file = new File("/home/gianluca/Programming/IdeaProjects/prova-alchemist-scafi/src/main/yaml/simple.yml")
+  val lock = new Semaphore(0)
   start(file)
-
   private def start[P <: Position[P]](file: File): Unit = {
     val env = LoadAlchemist.from(file).getDefault[Any, P]().getEnvironment
     val engine = new Engine(env)
@@ -26,10 +28,18 @@ object TestLauncher extends App {
                     reaction: Actionable[Any],
                     time: Time,
                     step: Long
-                  ): Unit =
+                  ): Unit = {
         if (time.toDouble >= stopWhen ) {
+          lock.release()
           engine.pause()
         }
+      }
+
+      override def finished(environment: Environment[Any, P], time: Time, step: Long): Unit = {}
+
+      override def initialized(environment: Environment[Any, P]): Unit = {
+        println("here..")
+      }
     }
 
     var outputMonitor = timeToPause(1)
@@ -37,20 +47,26 @@ object TestLauncher extends App {
 
     engine.play()
     new Thread(() => engine.run()).start()
-
+    new Thread(() => while(true) {
+      engine.play()
+      Thread.sleep(1000)
+    }).start()
     while (true) {
-      println("Paused")
-      engine.waitFor(Status.PAUSED, Long.MaxValue, TimeUnit.SECONDS)
-      println("Wake up")
+      lock.acquire()
+      //engine.waitFor(Status.PAUSED, Long.MaxValue, TimeUnit.SECONDS)
       val getTime = engine.getTime
       println(getTime)
       engine.removeOutputMonitor(outputMonitor)
       outputMonitor = timeToPause(getTime.toDouble + 1)
       engine.addOutputMonitor(outputMonitor)
+      // TODO FIX IN ALCHEMIST
       engine.play()
-      println("Here..")
-      engine.waitFor(Status.RUNNING, Long.MaxValue, TimeUnit.SECONDS)
-      println("Run again")
+      /*
+      do {
+        engine.play()
+        Thread.sleep(100)
+        engine.waitFor(Status.RUNNING, 1, TimeUnit.MILLISECONDS)
+      } while(engine.getStatus == Status.PAUSED)*/
     }
   }
 }
